f12e37a845efcd7ccd4e52ebdb81e7af6e0d56c3
itl12345 <157427657+itl12345@users.noreply.github.com>
2024-01-22 18:41:48 -0500

    Update js.js

--- js.js
+++ js.js
@@ -6,6 +6,7 @@ function calculate() {
           var select = document.getElementById("school");
           var selectedOption = select.options[select.selectedIndex]; 
           if (selectedOption.text == "Greenhills") {
+                    
                     bar()
           }
 }
@@ -14,24 +15,24 @@ function bar(){
           document.getElementById("bar").style.visibility = "unset";
           function myLoop() {         //  create a loop function
                     setTimeout(function() {   //  call a 3s setTimeout when the loop is called  //  your code here
-                              width++; 
-                              if (width == 100){
+                              width+=0.1; 
+                              if (width > 19 & width <21){
                                         document.getElementById("updates").innerHTML = "Gathering Snow Data..."
                               }
-                              else if (width == 200){
+                              else if (width > 39 & width <41){
                                         document.getElementById("updates").innerHTML = "Hacking Into Greenhills School..."
                               } 
-                              else if (width == 300){
+                              else if (width > 59 & width <61){
                                         document.getElementById("updates").innerHTML = "Retrieving Snow Day Info..."
                               } 
-                              else if (width == 400){
+                              else if (width > 79 & width <81){
                                         document.getElementById("updates").innerHTML = "Finding Percentage..."
                               } 
-                              else if (width == 500){
+                              else if (width > 99){
                                         document.getElementById("updates").innerHTML = "Success"
                               }             //  increment the counter
-                              if (width <= 502) {
-                                        document.getElementById("progress_bar").style.width = width.toString() + "px"           //  if the counter < 10, call the loop function
+                              if (width <= 100) {
+                                        document.getElementById("progress-bar").style.width = width.toString() + "%"           //  if the counter < 10, call the loop function
                                         myLoop();             //  ..  again which will trigger another 
                               }
                               else{
